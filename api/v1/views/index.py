224432d01f452389f9dc1dfd20415af33ce1aa42
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-18 13:52:43 -0400

    Adding new route and close method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 
 """Index module for the API"""
 
-from api.v1.views import app_views
+from api.v1.views import app_views, index
 from flask import jsonify
 from models import storage
 
@@ -12,3 +12,19 @@ from models import storage
 def status():
     """Return status"""
     return jsonify(status="OK")
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Return stats"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
+
+def count(cls):
+    """Count the number of objects in storage"""
+    return storage.count(cls)
