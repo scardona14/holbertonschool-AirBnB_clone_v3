58230df08c2b631d6aaf63f0a04f4c3ff83f46da
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-18 14:00:00 -0400

    Addig error msg

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,17 @@ from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import jsonify
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def not_found(exception):
+    """ Handles 404 """
+    return jsonify(error="Not found"), 404
 
 @app.teardown_appcontext
 def teardown_db(exception):
