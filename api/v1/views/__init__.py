52dc71b283fa38b4cef9851c3c9ad7f2bf51eab3
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-18 15:10:22 -0400

    fixing pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 """Module for API views"""
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 """ Blueprint object that handles all views for API v1"""
+
+
+from api.v1.views.index import *
+""" Importing index module for the API"""
