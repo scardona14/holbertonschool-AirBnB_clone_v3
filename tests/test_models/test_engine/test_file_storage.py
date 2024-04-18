84507bff820aa713dd9bfa0e5d5a690deda3e742
scardona14 <cardanasean123@gmail.com>
2024-04-18 11:45:17 -0400

    Adding file storage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,6 +18,7 @@ import json
 import os
 import pep8
 import unittest
+from models import storage
 FileStorage = file_storage.FileStorage
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -113,3 +114,27 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test method for obtaining an instance of the file storage"""
+        dic = {"name": "Puebla"}
+        isinstance = State(**dic)
+        storage.new(isinstance)
+        storage.save()
+        storage = FileStorage()
+        get_instance = storage.get(State, isinstance.id)
+        self.assertEqual(get_instance, isinstance)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test method for counting the number of objects in storage"""
+        dic = {"name": "Merida"}
+        state = State(**dic)
+        storage.new(state)
+        dic = {"name": "Mexico", "state_id": state.id}
+        city = City(**dic)
+        storage.new(city)
+        storage.save()
+        count = storage.count()
+        self.assertEqual(len(storage.all()), count)
