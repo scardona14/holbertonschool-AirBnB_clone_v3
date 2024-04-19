b95bae0c93008de298008ed8ec2e316db961b74f
scardona14 <cardanasean123@gmail.com>
2024-04-18 21:46:44 -0400

    Added the module for user

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,3 +9,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 """ Importing index module for the API"""
+
+
+from api.v1.views.users import *
+""" Importing users module for the API"""
