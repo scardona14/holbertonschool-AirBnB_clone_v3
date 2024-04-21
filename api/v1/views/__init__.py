f1f36d3d6cbc131adae7cd7650d2b9c5edf45c43
leomar <leomar.rodriguez1@upr.edu>
2024-04-21 15:08:28 -0400

    changed order of imports in init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,6 +10,9 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 """ Importing index module for the API"""
 
+from api.v1.views.states import *
+""" Imporitng states module for the API"""
+
 
 from api.v1.views.users import *
 """ Importing users module for the API"""
@@ -20,6 +23,3 @@ from api.v1.views.places_reviews import *
 
 from api.v1.views.places import *
 """ Importing places module for the API"""
-
-from api.v1.views.states import *
-""" Imporitng states module for the API"""
