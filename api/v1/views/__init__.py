6da6a5c79489749eb7901b10fb21805762f246b7
leomar <leomar.rodriguez1@upr.edu>
2024-04-21 15:28:08 -0400

    deleted documentation at init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
 
-"""Module for API views"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-""" Blueprint object that handles all views for API v1"""
-
 
 from api.v1.views.index import *
-""" Importing index module for the API"""
-
 
 from api.v1.views.users import *
-""" Importing users module for the API"""
-
 
 from api.v1.views.places_reviews import *
-""" Importing places module for the API"""
 
 from api.v1.views.places import *
-""" Importing places module for the API"""
 
 from api.v1.views.states import *
-""" Imporitng states module for the API"""
