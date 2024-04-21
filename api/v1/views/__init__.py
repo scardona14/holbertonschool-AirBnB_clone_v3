b6f7eb17b40c45c90982550365e3e8d3f631f445
leomar <leomar.rodriguez1@upr.edu>
2024-04-21 15:26:24 -0400

    database now fully working testing checkers

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,9 +10,6 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 """ Importing index module for the API"""
 
-from api.v1.views.states import *
-""" Imporitng states module for the API"""
-
 
 from api.v1.views.users import *
 """ Importing users module for the API"""
@@ -23,3 +20,6 @@ from api.v1.views.places_reviews import *
 
 from api.v1.views.places import *
 """ Importing places module for the API"""
+
+from api.v1.views.states import *
+""" Imporitng states module for the API"""
